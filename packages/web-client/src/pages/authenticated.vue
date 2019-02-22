49f5eea2d721e7fc38a785e1d3fd86d6d7f04869
gregory-latinier <gregory.latinier@gmail.com>
2019-02-22 01:51:34 +0100

    Move web to web-client Add admin website Add init admin user

---
+++ packages/web-client/src/pages/authenticated.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  name: 'page-authenticated'
+}
+</script>
+
+<template lang="pug">
+q-page.flex.flex-center
+  | Authenticated page
+</template>
+
+<style lang="stylus">
+
+</style>
