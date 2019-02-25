1428aa030b6e82137165d2b1cdf8c6e500d36f1e
gregory-latinier <gregory.latinier@gmail.com>
2019-02-25 16:35:58 +0100

    Add endpoint validation and error response formatting

--- packages/api/index.js
+++ packages/api/index.js
@@ -8,20 +8,34 @@ const server = Hapi.server({
   host: 'localhost',
   port: process.env.PORT || 5000,
   routes: {
-    cors: true
+    cors: true,
+    validate: {
+      failAction: (request, h, err) => {
+        throw err
+      }
+    }
   }
 })
-
-server.route({
-  method: 'GET',
-  path: '/hello',
-  handler: async (req,h) => {
-    const { User } = req.mongodb.models
-    const users = await User.find({ }).lean()
-    return h.response(users)
+server.ext('onPreResponse', (request, h) => {
+  const response = request.response
+  if (!response.isBoom) {
+    return h.continue
   }
-})
-
+  // There is a validation error
+  if (response.isJoi) {
+    const errors = []
+    response.details.forEach((error) => {
+      errors.push({
+        message: error.type,
+        field: error.context.key
+      })
+    })
+    const res = h.response(errors)
+    res.statusCode = 400
+    return res
+  }
+  return h.continue
+});
 const start =  async () => {
   try {
     await server.register(MongoDB)
