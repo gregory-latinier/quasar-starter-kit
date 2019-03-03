c63fad8403cf8777bacd56ca008662809a27fb4b
gregory-latinier <gregory.latinier@gmail.com>
2019-03-03 16:26:25 +0100

    Add async validation for email used rules

--- packages/web-admin/src/i18n/en-us/index.js
+++ packages/web-admin/src/i18n/en-us/index.js
@@ -3,6 +3,7 @@ export default {
     errors: {
       dateAfter: 'The "{after}" must be after "{before}"',
       email: 'Please type a valid email',
+      emailUsed: 'This email is already used',
       maxLength: 'Please type at most {count} characters',
       minLength: 'Please type at least {count} characters',
       required: 'This field is required'
